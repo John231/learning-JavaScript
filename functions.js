660e2e8603ed8638410f79d55ea9ba3c3150a103
John231 <j_b_93@hotmail.co.uk>
2017-10-11 15:51:52 +0200

    added arrow function expressions and closures

---
+++ functions.js
@@ -0,0 +1,77 @@
+/*
+ * Standard function expression
+ */
+console.log('=== Standard function expression ===');
+var funcDiv1 = document.createElement('div');
+var userInput = prompt('Enter text to appear on screen!');
+
+function addTextToDocument(text){
+    let textElement = document.createElement('p');
+    textElement.textContent = String(text);
+    funcDiv1.appendChild(textElement);
+}
+addTextToDocument(userInput);
+
+/*
+ * Anonymous function expression
+ */
+console.log('=== Anonymous function ===');
+var addText = function(text){
+    let textElement = document.createElement('p');
+    textElement.textContent = String(text);
+    funcDiv1.appendChild(textElement);
+}
+addText('Hey there, anonymous function usage!');
+document.body.appendChild(funcDiv1);
+
+/*
+ * Example of a closure
+ */
+console.log('=== Closures ===')
+var createCar = function(manufacturer){
+    return {
+        setManufacturer: function(newManufacturer) {
+            manufacturer = newManufacturer;
+        },
+        getManufacturer: function() {
+            return manufacturer;
+        }
+    }
+}
+var newCar = createCar('Aston Martin');
+console.log('This car is manufactured by ' + newCar.getManufacturer());
+newCar.setManufacturer('Ferrari');
+console.log('Manufacturer changed! New manufacturer is: ' + newCar.getManufacturer());
+
+/*
+ * Usage of the arguments object
+ */
+function concatenator(separator) {
+   let result = '';
+   let i;
+   for (i = 1; i < arguments.length; i++) {
+      result += arguments[i] + separator;
+}
+   return result;
+}
+console.log(concatenator(', ', 'sage', 'basil', 'oregano', 'pepper', 'parsley'));
+
+/*
+ * Default parameter
+ */
+function multiply(a,b=1){
+    return a*b;
+}
+console.log('3 x 2 = ' + multiply(3,2));
+console.log('5 = '+ multiply(5));
+
+/*
+ * Arrow functions
+ */
+
+var elementArray = ['Hydrogen','Helium','Lithium','Beryllium'];
+
+// Comparison of closure styles and lambda functions in ES3/5 and ES6
+var a2 = elementArray.map(function(s){return s.length;});
+// ES6 style (using arrow function expression)
+var a3 = elementArray.map(s => s.length);
