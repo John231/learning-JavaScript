3cce4c6783bd1e8da6a6a8ce5dccd04ddebbf9c3
John231 <j_b_93@hotmail.co.uk>
2017-10-11 10:37:07 +0200

    Addition of loops-and-iteration.html

---
+++ control-flow.js
@@ -0,0 +1,57 @@
+// an example if statement
+function checkIsTrue(testBool){
+    if (testBool){
+        console.log('Evaluated as true!');
+    } else {
+        console.log('Evaluated as false!');
+    }
+}
+
+// an example switch statement
+function getManufacturerDescription(carManufacturer){
+    switch (carManufacturer){
+        case 'mercedes' :
+            console.log('Mercedes-Benz ist eine eingetragene Handelsmarke für Fahrzeuge der Daimler AG');
+            break;
+        case 'bmw' :
+            console.log('Die Bayerische Motoren Werke Aktiengesellschaft ist die Muttergesellschaft der BMW Group, eines weltweit operierenden deutschen Automobil- und Motorradherstellers mit Sitz in München');
+            break;
+        default :
+            console.log('Autohersteller nicht gefunden');
+    }
+}
+
+// an example throw statement
+function checkForError(testString){
+    if (testString === 'error'){
+        throw 'ERROR!';
+    } else {
+        console.log('no errors found');
+    }
+}
+
+// an example error handling try-catch (finally) block
+function exampleErrorHandler(){
+    var userMonth =  Number(prompt('Please enter the number of the desired month'));
+    userMonth -= 1; // adjust to match array index location
+
+    try {
+        console.log(getMonth(userMonth));
+    } catch (e) {
+        // print the error to the console
+        console.log(e);
+    } finally {
+        console.log('Executed after try-catch')
+    }
+}
+// function which throws an error if the index is out of range
+function getMonth(monthIdx){
+    var months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']; 
+    if (months[monthIdx]){
+        // return the corresponding month as a string
+        return months[monthIdx];
+    } else {
+        // throw an error (String)
+        throw 'Month Not Found';
+    }
+}
---
+++ notepad.js
@@ -0,0 +1,97 @@
+/*
+ * Dies ist eine JavaScript-Umgebung.
+ *
+ * Geben Sie etwas JavaScript ein und führen Sie einen Rechtsklick aus oder wählen Sie aus dem Ausführen-Menü:
+ * 1. Ausführen, um den ausgewählten Text zu evaluieren (Cmd-R),
+ * 2. Untersuchen, um den Objekt-Inspektor auf das Resultat anzuwenden (Cmd-I), oder
+ * 3. Anzeigen, um das Ergebnis in einem Kommentar hinter der Auswahl einzufügen. (Cmd-L)
+ */
+
+function greetMe(name){
+    alert('Hello ' + name);
+}
+greetMe('world!');
+
+/* Declaration types:
+ * var - variable with global or local scope
+ * let - block-scoped, local variable
+ * const - block-scoped, read-only named constant
+ */
+
+
+// variable declaration:
+var myString = 'hagrid';
+
+// undefined is used to determine whether a variable has been assigned a value
+var input;
+if (input === undefined){
+    alert('input was not assigned!');
+}
+
+// variable scope - var is local to function scope:
+if(true){
+    var y = 5;
+}
+console.log(y);
+
+if(true){
+    // cannot be reached outside of if statement
+    var num = 6;
+    console.log(num)
+}
+
+// hoisting means that variables are hoisted to top of a function statement (only applicable to vars and not lets or consts)
+console.log('My hoisted variable is: ' + hoistedVar);
+var hoistedVar = 7;
+
+// take care with the namespace, functions and global variables MUST NOT have the same name
+
+// parsing strings into number data type
+var base = 10; // radix or number system to be used!
+var myStringNumber = "6";
+var numberFromString = Number.parseInt(myStringNumber, base);
+console.log('My number parsed from string is: ' + numberFromString);
+
+var myStringDecimal = '1.2';
+var decimalFromString = Number.parseFloat(myStringDecimal, base);
+console.log('My decimal parsed from String is: ' + decimalFromString);
+
+// alternative method for retrieving a number from string:
+var unaryPlusFun = (+myStringDecimal);
+console.log('Unary plus operator returns type ' + unaryPlusFun.type);
+
+// Array literals
+var coffees = ['French roast', 'Java', 'Kona'];
+console.log('The first element of the coffee array is: ' + coffees[0]);
+
+// if array elements are empty, they will be evaluated as undefined
+var fish = ['Trout', , 'Salmon', 'Sole'];
+console.log('The second element of the fish array is: ' + fish[1]);
+// note this will not work for trailing commas!! eg. fish = ['x','y',];
+// best practice is to define empty elements as undefined! eg.
+var improvedFish = ['Trout', undefined, 'Salmon', 'Sole'];
+
+//floating point literals
+var anInt = 3;
+var aFloat = 3.2;
+var aFloatExp = -2.45e+2;
+
+// object literals
+var carObject = {make:'ferrari', color: getColor('ferrari'), interiorOptions: {basic: 'uncomfortable seats', racing: 'bucket seats', deluxe: 'leather seats'}};
+
+function getColor(car){
+    switch (car) {
+        case 'ferrari':
+            return 'red'
+            break;
+        default:
+            console.log('not found');
+            return undefined;
+    }
+};
+
+console.log('This car was manufactured by ' + carObject['make'] + ' and is ' + carObject.color);
+console.log('The racing package for this car comes fitted with ' + carObject.interiorOptions['racing']);
+
+// string literals
+var newLineString = 'I\'m on the line above \n I\'m on the line below';
